63e30e8706e436bb6e163051faf003760ba3b57b
Dansmith-rgb <68809853+Dansmith-rgb@users.noreply.github.com>
2020-08-30 21:02:14 +0100

    Add files via upload
    adding a color to the background.

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,13 +12,19 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and nouse events.
-            for event in pygame.evnt.get():
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
