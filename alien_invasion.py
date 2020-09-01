17bc6c5d6ade7c06855a6ec066779158f64dbc71
Dansmith-rgb <68809853+Dansmith-rgb@users.noreply.github.com>
2020-09-01 15:45:09 +0100

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,8 +31,9 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+
 
 
     def _check_events(self):
@@ -65,9 +66,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
-            
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update the position of the bullets and get rif of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        # Get rid of the bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+                    
     def _update_screen(self):
         """Update the imgaes on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
