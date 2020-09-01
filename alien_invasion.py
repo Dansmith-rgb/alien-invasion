53b278340fd95694096e28d0d5e68ccbfa653feb
Dansmith-rgb <68809853+Dansmith-rgb@users.noreply.github.com>
2020-09-01 09:39:22 +0100

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -39,9 +39,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                    
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update the imgaes on the screen, and flip to the new screen."""
