2f6da3b29ab9a180652549f12bff38aa70707bfe
Dansmith-rgb <68809853+Dansmith-rgb@users.noreply.github.com>
2020-09-01 16:27:56 +0100

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """Overall class to manage the game assets and behavior."""
@@ -19,8 +20,10 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
-        
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -71,13 +74,19 @@ class AlienInvasion:
             self.bullets.add(new_bullet)
 
     def _update_bullets(self):
-        """Update the position of the bullets and get rif of old bullets."""
+        """Update the position of the bullets and get rid of old bullets."""
         # Update bullet positions.
         self.bullets.update()
         # Get rid of the bullets that have disappeared
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Make an alien.
+        alien = Alien(self)
+        self.aliens.add(alien)
                     
     def _update_screen(self):
         """Update the imgaes on the screen, and flip to the new screen."""
@@ -85,6 +94,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw()
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
