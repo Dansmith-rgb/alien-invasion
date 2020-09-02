795e214f0082278e02ec3628018e839820f289d6
Dansmith-rgb <68809853+Dansmith-rgb@users.noreply.github.com>
2020-09-02 09:02:07 +0100

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,6 +82,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        # Check for any bullets that have hit aliens.
+        #  If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
 
     def _update_aliens(self):
         """
