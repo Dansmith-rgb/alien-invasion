3daba4a7908b5d4cae605446a58aa1da2f026563
Dansmith-rgb <68809853+Dansmith-rgb@users.noreply.github.com>
2020-09-04 07:11:26 +0100

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -33,7 +33,9 @@ class Scoreboard:
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
         high_score_str = "{:,}".format(high_score)
-        self.high_score_rect = self.font.render(high_score_image.get_rect())
+        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        
+        self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
 
